d2913ea36d8dbe23c883ad19dc13a41ae7bd321f
Vitaly Shonov <v-shonov@mail.ru>
2017-06-02 15:50:12 +0300

    created email activation

--- clients/forms.py
+++ clients/forms.py
@@ -1,10 +1,13 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.db import models
 from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Электронный адрес'}))
 
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('username', 'last_name', 'first_name', 'email',)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
