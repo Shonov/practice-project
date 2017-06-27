c710f974a37ecb2f423ea0ce02aabfdb8c8e61dd
Vitaly Shonov <v-shonov@mail.ru>
2017-06-27 07:39:04 +0300

    attempt to create comments

--- public/static/clients/ajax.js
+++ public/static/clients/ajax.js
@@ -1,25 +1,26 @@
-$().ready( function(){
-    $("a#item_like").click(function (e) {
+$().ready(function () {
+
+    $("p#item_like").click(function (e) {
         e.preventDefault();
 
         let id = $(this).attr("data-id");
-        let full_id = '#id_like_' + id;
-        let message = '#message_' + id;
+        let full_id = `#id_like_${id}`;
 
         $.ajax({
             url: 'like/',
             type: 'GET',
             dataType: 'json',
             data: {'client_id': id},
-            success: function (data) {
+            success: (data) => {
                 $(full_id).html(data['like']);
-                console.log(data);
-                $(message).html(data.message);
-            }
-            // error: function(data, textStatus) {
-            //     alert([data.status, textStatus]);
-            // },
+                if (data.message) {
+                    toastr.error(data.message);
+                }
 
+            },
+            error: (data, textStatus) => {
+                alert(data.status, textStatus)
+            }
         });
         return false;
     });
